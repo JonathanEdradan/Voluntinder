dcd05fb4b32beb8906d9c4e80b416b82f92d601b
Jonathan Edradan <jmedradan@gmail.com>
2015-01-25 23:06:40 -0800

    added columns to user table and TDD them

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150126061743) do
+ActiveRecord::Schema.define(version: 20150126064812) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                          null: false
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20150126061743) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.integer  "zipcode"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,29 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   describe 'Attributes' do
-  	 it { is_expected.to have_db_column :first_name }
-  	 it { is_expected.to have_db_column :last_name }
-  	 it { is_expected.to have_db_column :address }
-  	 it { is_expected.to have_db_column :city }
-  	 it { is_expected.to have_db_column :state }
-  	 it { is_expected.to have_db_column :zipcode }
+  	 it { is_expected.to respond_to :first_name }
+  	 it { is_expected.to respond_to :last_name }
+  	 it { is_expected.to respond_to :address }
+  	 it { is_expected.to respond_to :city }
+  	 it { is_expected.to respond_to :state }
+  	 it { is_expected.to respond_to :zipcode }
+  end
+
+  describe 'Database' do
+ 		it { is_expected.to have_db_column :first_name }
+  	it { is_expected.to have_db_column :last_name }
+  	it { is_expected.to have_db_column :address }
+  	it { is_expected.to have_db_column :city }
+  	it { is_expected.to have_db_column :state }
+  	it { is_expected.to have_db_column :zipcode }
+  end
+
+  describe 'Validations' do
+  	it { is_expected.to validate_presence_of :first_name }
+  	it { is_expected.to validate_presence_of :last_name }
+  	it { is_expected.to validate_presence_of :address }
+  	it { is_expected.to validate_presence_of :city }
+  	it { is_expected.to validate_presence_of :state }
+  	it { is_expected.to validate_presence_of :zipcode }
   end
 end
